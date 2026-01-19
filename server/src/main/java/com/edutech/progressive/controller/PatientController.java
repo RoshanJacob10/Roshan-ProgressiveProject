d325ffc63c66a527f6a8a7bd9bf680a947c13833
RoshanJacob10 <dummy@gmail.com>
2026-01-19 13:12:43 +0000

    Day 4 Task

--- server/src/main/java/com/edutech/progressive/controller/PatientController.java
+++ server/src/main/java/com/edutech/progressive/controller/PatientController.java
@@ -2,9 +2,12 @@ package com.edutech.progressive.controller;
 
 import com.edutech.progressive.entity.Patient;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@RestController
+@RequestMapping("/api/patients")
 public class PatientController {
 
     public ResponseEntity<List<Patient>> getAllPatients() {
